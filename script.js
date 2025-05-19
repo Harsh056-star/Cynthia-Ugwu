5759fad1ac729813f9d8c4514bfda4e337f1fbd5
Harsh056-star <hnagpal056@gmail.com>
2025-05-20 00:06:57 +0530

    eight change

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-import LocomotiveScroll from 'locomotive-scroll';
-
 const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),//el means element aur ye main part pr apply hoga jisme saari website h
+    el: document.querySelector('#main'),
     smooth: true
 });
+//el means element aur ye main part pr apply hoga jisme saari website h
