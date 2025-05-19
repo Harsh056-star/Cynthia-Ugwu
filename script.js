a650a6fa6b8718baad1347b8da023a4dd1916e8e
Harsh056-star <hnagpal056@gmail.com>
2025-05-20 00:38:45 +0530

    smooth scrolling done

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
+    el: document.querySelector('.main'),
     smooth: true
 });
 //el means element aur ye main part pr apply hoga jisme saari website h
