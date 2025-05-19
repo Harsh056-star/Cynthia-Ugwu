e3c3e942e2c8a65365d886a33b27f2a405346a0f
Harsh056-star <hnagpal056@gmail.com>
2025-05-20 01:01:02 +0530

    ninth commit

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ const scroll = new LocomotiveScroll({
     smooth: true
 });
 //el means element aur ye main part pr apply hoga jisme saari website h
+function cursor(){
+    window.addEventListener("mousemove", function(dets){
+        // console.log(dets.clientX, dets.clientY);ye sara classify krta h kese cursor move hota h
+        document.querySelector("#cursor").style.transform =`translate(${dets.clientX}px, ${dets.clientY}px)`
+    })
+}
+cursor();
