906937bd3125465e2807e872273dd9cb6e781a51
Harsh056-star <hnagpal056@gmail.com>
2025-05-19 23:50:14 +0530

    eight commit

---
+++ script.js
@@ -0,0 +1,6 @@
+import LocomotiveScroll from 'locomotive-scroll';
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),//el means element aur ye main part pr apply hoga jisme saari website h
+    smooth: true
+});
