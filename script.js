f296884c41cacffae0dd78bb08d9f9c4637c2477
Harsh056-star <hnagpal056@gmail.com>
2025-05-20 13:25:27 +0530

    Tenth commit

--- script.js
+++ script.js
@@ -3,6 +3,22 @@ const scroll = new LocomotiveScroll({
     smooth: true
 });
 //el means element aur ye main part pr apply hoga jisme saari website h
+function firstPageAnim() {
+  var tl = gsap.timeline();
+
+  tl.from("#nav", {
+    y: "-10",
+    opacity: 0,
+    duration: 1.5,
+    ease: Expo.easeInOut,
+  })
+  .to(".boundingmove", {
+    y: 0,
+    ease: Expo.easeInOut,
+    duration: 2,
+    stagger: .2,
+  })
+}
 function cursor(){
     window.addEventListener("mousemove", function(dets){
         // console.log(dets.clientX, dets.clientY);ye sara classify krta h kese cursor move hota h
@@ -10,3 +26,4 @@ function cursor(){
     })
 }
 cursor();
+firstPageAnim();
